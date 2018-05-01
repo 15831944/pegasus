a6df72ebbb38ff12c2928af33c933b8faf4d1d51
Shubham Shrivastava <shubham.shrivastava@testorigen.com>
2018-05-01 14:15:16 +0530

    First commit

---
+++ pegqa1/Pegasus_New_skin/Scripts/List Management/AdvanceFilterDetailedPayout.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PegasusTests.PageHelper;
+using PegasusTests.PageHelper.Comm;
+
+namespace PegasusTests.Scripts.Corp
+{
+    [TestClass]
+    public class AdvanceFilterDetailedPayout : DriverTestCase
+    {
+        [TestMethod]
+        [TestCategory("All")]
+        [TestCategory("Bug")]
+        [TestCategory("TS2")]
+        [TestCategory("BugTestNew")]
+        public void advanceFilterDetailedPayout()
+        {
+            string[] username1 = null;
+            string[] password1 = null;
+
+            var oXMLData = new XMLParse();
+            oXMLData.LoadXML("../../Config/ApplicationSettings.xml");
+            username1 = oXMLData.getData("settings/Credentials", "username_corp");
+            password1 = oXMLData.getData("settings/Credentials", "password");
+
+            // Initializing the objects
+            var executionLog = new ExecutionLog();
+            var loginHelper = new LoginHelper(GetWebDriver());
+            var residualIncome_OfficePayout_DetailedPayoutHelper = new ResidualIncome_OfficePayout_DetailedPayoutHelper(GetWebDriver());
+
+            // Variable random
+            String JIRA = "";
+            String Status = "Pass";
+
+            //try
+            //{
+            executionLog.Log("AdvanceFilterDetailedPayout", "Login with valid username and password");
+            Login(username1[0], password1[0]);
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Verify Page title");
+            VerifyTitle("Dashboard");
+            Console.WriteLine("Redirected at Dashboard screen.");
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Redirect To Detailed Payout page");
+            GetWebDriver().Navigate().GoToUrl("https://www.pegasus-test.com/newthemecorp/pegasustestoffice/rir/detailed_payouts");
+            residualIncome_OfficePayout_DetailedPayoutHelper.WaitForWorkAround(2000);
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Click on advance filter");
+            residualIncome_OfficePayout_DetailedPayoutHelper.ClickElement("Advance");
+            residualIncome_OfficePayout_DetailedPayoutHelper.WaitForWorkAround(1000);
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Enter Filter Name");
+            residualIncome_OfficePayout_DetailedPayoutHelper.TypeText("Filtername", "testerqa");
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Click Update Button");
+            residualIncome_OfficePayout_DetailedPayoutHelper.ClickElement("Update");
+            residualIncome_OfficePayout_DetailedPayoutHelper.WaitForWorkAround(3000);
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Click on advance filter");
+            residualIncome_OfficePayout_DetailedPayoutHelper.ClickElement("Advance");
+
+            executionLog.Log("AdvanceFilterDetailedPayout", "Verify Filter Name in Saved Filter");
+            residualIncome_OfficePayout_DetailedPayoutHelper.OptionNotPresentInDropdown("SavedFilter", "testerqa");
+
+
+            //}
+            //catch (Exception e)
+            //{
+            //    executionLog.Log("Error", e.StackTrace);
+            //    Status = "Fail";
+
+            //    String counter = executionLog.readLastLine("counter");
+            //    String Description = executionLog.GetAllTextFile("AdvanceFilterDetailedPayout");
+            //    String Error = executionLog.GetAllTextFile("Error");
+            //    if (counter == "")
+            //    {
+            //        counter = "0";
+            //    }
+            //    bool result = loginHelper.CheckExstingIssue("Advance Filter Detailed Payout");
+            //    if (!result)
+            //    {
+            //        if (Int16.Parse(counter) < 9)
+            //        {
+            //            executionLog.Count("counter", (Int16.Parse(counter) + 1).ToString());
+            //            loginHelper.CreateIssue("Advance Filter Detailed Payout", "Bug", "Medium", "Corp Employee page", "QA", "Log in as: " + username1[0] + " / " + password1[0] + "\n\nSteps:\n" + Description + "\n\n\nError Description:\n" + Error);
+            //            string id = loginHelper.getIssueID("Advance Filter Detailed Payout");
+            //            TakeScreenshot("AdvanceFilterDetailedPayout");
+            //            string directoryName = loginHelper.GetnewDirectoryName(GetPath());
+            //            var location = directoryName + "\\AdvanceFilterDetailedPayout.png";
+            //            loginHelper.AddAttachment(location, id);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (Int16.Parse(counter) < 9)
+            //        {
+            //            executionLog.Count("counter", (Int16.Parse(counter) + 1).ToString());
+            //            TakeScreenshot("AdvanceFilterDetailedPayout");
+            //            string id = loginHelper.getIssueID("Advance Filter Detailed Payout");
+            //            string directoryName = loginHelper.GetnewDirectoryName(GetPath());
+            //            var location = directoryName + "\\AdvanceFilterDetailedPayout.png";
+            //            loginHelper.AddAttachment(location, id);
+            //            loginHelper.AddComment(loginHelper.getIssueID("Advance Filter Detailed Payout"), "This issue is still occurring");
+            //        }
+            //    }
+            //    JIRA = loginHelper.getIssueID("Advance Filter Detailed Payout");
+            // //   executionLog.DeleteFile("Error");
+            //    throw;
+
+            //}
+            //finally
+            //{
+            //    executionLog.DeleteFile("AdvanceFilterDetailedPayout");
+            //    executionLog.WriteInExcel("Advance Filter Detailed Payout", Status, JIRA, "Corp Employee");
+            //}
+            }
+    }
+}
---
+++ pegqa1/Pegasus_New_skin/Scripts/List Management/CustomColumnsLead.cs
@@ -0,0 +1,143 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using PegasusTests.PageHelper;
+using PegasusTests.PageHelper.Comm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace PegasusTests.Scripts.ClientsTests
+{
+    [TestClass]
+    public class CustomColumnsLead : DriverTestCase
+    {
+        [TestMethod]
+        [TestCategory("All")]
+        [TestCategory("Bug")]
+        [TestCategory("TS1")]
+        [TestCategory("BugTest")]
+        public void customColumnsLead()
+        {
+            string[] username = null;
+            string[] password = null;
+
+            XMLParse oXMLData = new XMLParse();
+            oXMLData.LoadXML("../../Config/ApplicationSettings.xml");
+
+            username = oXMLData.getData("settings/Credentials", "username_office");
+            password = oXMLData.getData("settings/Credentials", "password");
+
+            // Initializing the objects
+            var executionLog = new ExecutionLog();
+            var loginHelper = new LoginHelper(GetWebDriver());
+            var listManagementHelper = new ListManagementHelper(GetWebDriver());
+
+            // Variable 
+            var name = "Test" + GetRandomNumber();
+            var name2 = "Testlist" + GetRandomNumber();
+            var Id = "12345" + GetRandomNumber();
+            String JIRA = "";
+            String Status = "Pass";
+
+            //try
+            //{
+                executionLog.Log("CustomColumnsLead", "Login with valid username and password");
+                Login(username[0], password[0]);
+                Console.WriteLine("Logged in as: " + username[0] + " / " + password[0]);
+
+                executionLog.Log("CustomColumnsLead", "Verify Page title");
+                VerifyTitle("Dashboard");
+                Console.WriteLine("Redirected at Dashboard screen.");
+
+                executionLog.Log("CustomColumnsLead", "Redirect To List Management page");
+                listManagementHelper.ClickElement("Marketing");
+                listManagementHelper.WaitForWorkAround(4000);
+
+                executionLog.Log("CustomColumnsLead", "Redirect To List Management page");
+                GetWebDriver().Navigate().GoToUrl("https://www.pegasus-test.com/en/listmanagements/leads");
+                listManagementHelper.WaitForWorkAround(2000);
+
+                executionLog.Log("CustomColumnsLead", "Click on Settings icon");
+                listManagementHelper.ClickForce("SettingIcon");
+                listManagementHelper.WaitForWorkAround(2000);
+
+                executionLog.Log("CustomColumnsLead", "Click on Search Box");
+                listManagementHelper.TypeText("SearchBox", "Account Manager");
+                listManagementHelper.WaitForWorkAround(1000);
+
+                executionLog.Log("CustomColumnsLead", "Click on Plus icon");
+                listManagementHelper.ClickElement("PlusIcon");
+                listManagementHelper.WaitForWorkAround(1000);
+
+                executionLog.Log("CustomColumnsLead", "Clear Search Field value");
+                listManagementHelper.ClearTextBoxValue("//input[@id='searchHeaderColumns']");
+                listManagementHelper.WaitForWorkAround(1000);
+
+                //executionLog.Log("CustomColumnsLead", "Click on Cross icon");
+                //listManagementHelper.ClickElement("CrossIcon");
+                //listManagementHelper.WaitForWorkAround(1000);
+
+                executionLog.Log("CustomColumnsLead", "Click on Apply Button");
+                listManagementHelper.ClickForce("Apply");
+                listManagementHelper.WaitForWorkAround(1000);
+
+                //executionLog.Log("CustomColumnsLead", "Verify the Option in Display Column");
+                //listManagementHelper.VerifyText("//*[@id='modalHeaderColumnsright']/div[10]/div/p", "Account Manager");
+                //listManagementHelper.WaitForWorkAround(1000);
+
+
+                }
+            //}
+            //catch (Exception e)
+            //{
+            //    executionLog.Log("Error", e.StackTrace);
+            //    Status = "Fail";
+
+            //    String counter = executionLog.readLastLine("counter");
+            //    String Description = executionLog.GetAllTextFile("CustomColumnsLead");
+            //    String Error = executionLog.GetAllTextFile("Error");
+            //    if (counter == "")
+            //    {
+            //        counter = "0";
+            //    }
+            //    bool result = loginHelper.CheckExstingIssue("Rename ListManagement Item");
+            //    if (!result)
+            //    {
+            //        if (Int16.Parse(counter) < 9)
+            //        {
+            //            executionLog.Count("counter", (Int16.Parse(counter) + 1).ToString());
+            //            loginHelper.CreateIssue("Rename ListManagement Item", "Bug", "Medium", "Equipment page", "QA", "Log in as: " + username[0] + " / " + password[0] + "\n\nSteps:\n" + Description + "\n\n\nError Description:\n" + Error);
+            //            string id = loginHelper.getIssueID("Rename ListManagement Item");
+            //            TakeScreenshot("CustomColumnsLead");
+            //            string directoryName = loginHelper.GetnewDirectoryName(GetPath());
+            //            var location = directoryName + "\\CustomColumnsLead.png";
+            //            loginHelper.AddAttachment(location, id);
+            //        }
+            //    }
+            //    else
+            //    {
+            //        if (Int16.Parse(counter) < 9)
+            //        {
+            //            executionLog.Count("counter", (Int16.Parse(counter) + 1).ToString());
+            //            TakeScreenshot("CustomColumnsLead");
+            //            string id = loginHelper.getIssueID("Rename ListManagement Item");
+            //            string directoryName = loginHelper.GetnewDirectoryName(GetPath());
+            //            var location = directoryName + "\\CustomColumnsLead.png";
+            //            loginHelper.AddAttachment(location, id);
+            //            loginHelper.AddComment(loginHelper.getIssueID("Rename ListManagement Item"), "This issue is still occurring");
+            //        }
+            //    }
+            //    JIRA = loginHelper.getIssueID("Rename ListManagement Item");
+            //    //  executionLog.DeleteFile("Error");
+            //    throw;
+
+            //}
+            //finally
+            //{
+            //    executionLog.DeleteFile("RenameListManagement Item");
+            //    executionLog.WriteInExcel("Rename ListManagement Item", Status, JIRA, "List Management");
+            //}
+        }
+    }
